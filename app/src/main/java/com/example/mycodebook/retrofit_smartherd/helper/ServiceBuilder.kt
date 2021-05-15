5a218a5ec328247bfab5195fcb4893fa86423c8c
Prateek <yprateek51@gmail.com>
2021-05-15 16:08:44 +0530

    logging interceptor is added

--- app/src/main/java/com/example/mycodebook/retrofit_smartherd/helper/ServiceBuilder.kt
+++ app/src/main/java/com/example/mycodebook/retrofit_smartherd/helper/ServiceBuilder.kt
@@ -2,6 +2,7 @@ package com.example.mycodebook.retrofit_smartherd.helper
 
 import okhttp3.OkHttp
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
@@ -9,14 +10,19 @@ import java.util.concurrent.TimeUnit
 object ServiceBuilder {
 
 
-    private const val URL =  "http:192.168.43.176//127.0.0.1:3000/"
+    private const val URL =  "http://127.0.0.1:3000/"
+
+
+    private val  logger =  HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
 
     private var okHttp = OkHttpClient.Builder().also {
         it.callTimeout(2,TimeUnit.MINUTES)
         it.connectTimeout(20,TimeUnit.SECONDS)
         it.readTimeout(30,TimeUnit.SECONDS)
         it.writeTimeout(30,TimeUnit.SECONDS)
+        it.addInterceptor(logger)
     }
+
 
 
     //create retrofit builder
