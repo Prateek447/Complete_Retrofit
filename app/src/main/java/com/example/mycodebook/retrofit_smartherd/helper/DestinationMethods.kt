3e3f5d16078a6eb535b0530236221138bf2b0d1b
Prateek <yprateek51@gmail.com>
2021-05-15 17:02:37 +0530

    Query parameter addded

--- app/src/main/java/com/example/mycodebook/retrofit_smartherd/helper/DestinationMethods.kt
+++ app/src/main/java/com/example/mycodebook/retrofit_smartherd/helper/DestinationMethods.kt
@@ -4,14 +4,21 @@ import com.example.mycodebook.retrofit_smartherd.models.Destination
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface DestinationMethods {
 
-
+ //method 1: get destination simple
     @GET("destination")
     fun  getDestination() : Call<List<Destination>>
 
+  //method 2; get destination according to destination id
     @GET("destination/{id}")
     fun getDestinationById(@Path("id") id:Int): Call<Destination>
+
+    //method 3: get destination according to some condition like get destination where country is india
+    // if we didn't pass null while calling this method then this will work similar to method 1
+    @GET("destination")
+    fun getDestinationByCond(@Query("country") country : String? ) : Call<List<Destination>>
 
 }
